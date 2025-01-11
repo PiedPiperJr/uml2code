232f349a044e39b80e6a50314d9c2b3e79f551a7
Nameless0l <wwwmbassiloic@gmail.com>
2025-01-11 14:41:04 +0100

    update app structure and write specs

--- injectors/relationship_injector.py
+++ injectors/relationship_injector.py
@@ -5,35 +5,35 @@ from models.relationship_model import RelationshipType
 
 def inject_relationships(interpreted_data: Dict, relationships: Dict) -> None:
     for class_data in interpreted_data:
-            list_used: bool = False
-            class_data['aggregations'] = []
-            class_data['compositions'] = []
+        list_used: bool = False
+        class_data['aggregations'] = []
+        class_data['compositions'] = []
 
-            for relationship in relationships:
-                if class_data['type'] == "class" and class_data['name'].lower() == relationship.source_name.lower():
-                    match(relationship._type):
-                        case RelationshipType.INHERITANCE:
-                            class_data['parent'] = relationship.target_name
+        for relationship in relationships:
+            if class_data['type'] == "class" and class_data['name'].lower() == relationship.source_name.lower():
+                match(relationship._type):
+                    case RelationshipType.INHERITANCE:
+                        class_data['parent'] = relationship.target_name
 
-                        case RelationshipType.AGGREGATION:
-                            class_data['aggregations'].append({'visibility':'private', 
-                                                            'type':relationship.target_name.capitalize(),
-                                                            'name':relationship.target_name.lower() + 's'})
+                    case RelationshipType.AGGREGATION:
+                        class_data['aggregations'].append({'visibility':'private', 
+                                                        'type':relationship.target_name.capitalize(),
+                                                        'name':relationship.target_name.lower() + 's'})
 
-                        case RelationshipType.COMPOSITION:
-                            class_data['compositions'].append({'visibility':'private', 
-                                                            'type':relationship.target_name.capitalize(),
-                                                            'name':relationship.target_name.lower() + 's'})
-                        
-                        case RelationshipType.ATTRIBUTE:
-                            class_data['attributes'].append({'visibility':'private', 
-                                                            'type':relationship.target_name.capitalize(),
-                                                            'name':relationship.target_name.lower()})
+                    case RelationshipType.COMPOSITION:
+                        class_data['compositions'].append({'visibility':'private', 
+                                                        'type':relationship.target_name.capitalize(),
+                                                        'name':relationship.target_name.lower() + 's'})
+                    
+                    case RelationshipType.ATTRIBUTE:
+                        class_data['attributes'].append({'visibility':'private', 
+                                                        'type':relationship.target_name.capitalize(),
+                                                        'name':relationship.target_name.lower()})
 
-                        case RelationshipType.NONE:
-                            pass
+                    case RelationshipType.NONE:
+                        pass
 
-                    list_used = True
+                list_used = True
 
-            if list_used:
-                class_data['import_list'] = True
+        if list_used:
+            class_data['import_list'] = True
