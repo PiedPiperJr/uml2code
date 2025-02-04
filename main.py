ecb9418b848f04a06ad7146cf50a71fd4e806e80
Pacome Kengali Fegue <63215302+PacomeKFP@users.noreply.github.com>
2025-02-04 07:52:23 +0000

    add app attribute in main

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def create_app():
     app.register_blueprint(routes_bp)
     return app
 
+app = create_app() 
 if __name__ == '__main__':
-    app = create_app() 
     app.run(debug=True, host="0.0.0.0")
